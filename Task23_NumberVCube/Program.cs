2d26a6927c5fe4c3ad61c385f08fc017439d0785
Natali <nbyka@mail.ru>
2023-04-02 19:07:07 +0300

    NumberVCube

---
+++ Task23_NumberVCube/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+NumberVCube();
+
+int Read()
+{
+    Console.Write("Введите число: ");
+    int N = int.Parse(Console.ReadLine());
+    return N;
+}
+void NumberVCube()
+{
+    int num = 1;
+    int N = Read();
+    while(num <= N)
+    {
+        double n = Math.Pow(num, 3);
+        Console.Write(n+" ");
+        num++;
+    }
+}
