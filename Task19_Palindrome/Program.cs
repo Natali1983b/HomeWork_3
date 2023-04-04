4bafaf8adc5e8cd2e8577c57b5df42db1617f7cd
Natali <nbyka@mail.ru>
2023-04-04 23:22:30 +0300

    Palindromend

--- Task19_Palindrome/Program.cs
+++ Task19_Palindrome/Program.cs
@@ -1,36 +1,37 @@
 ﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Task(Read());
 
 int Read()
 {
-    Console.WriteLine("Введите число: ");
+    Console.Write("Введите число: ");
     int Number = int.Parse(Console.ReadLine());
-    Console.WriteLine(IsPalindrome(num1, num2, num3, num4));
-    
-    if(Number >= 10000 && Number < 100000)
-    {
-        Console.WriteLine(Number);
-    }
-    else
-    {
-        Console.WriteLine("Ошибка, введите пятизначное число");
-    }  
     return Number;
 }
-int Number = Read();
 
-int num1 = Number % 10;
-int num2 = Number / 1000 - 10;
-int num3 = Number % 100 / 10;
-int num4 = Number / 10000;
 
-int IsPalindrome(int num1, int num2, int num3, int num4)
+void Task(int Number)
 {
-    if(num1 == num4 && num2 == num3)
+    if(Number >= 10000 && Number < 100000)
     {
-       Console.WriteLine("Да, это палиндром");
+        int reverse = 0;
+        int num = Number;
+        while(Number > 0)
+        {
+            reverse = reverse * 10 + Number % 10;
+            Number = Number / 10;
+        } 
+        if(reverse == num)
+        {
+            Console.WriteLine("Да, это палиндром");
+        }
+        else
+        {
+            Console.WriteLine("Это не палиндром");
+        }
     }
     else
     {
-        Console.WriteLine("Это не палиндром");
+      Console.WriteLine("Ошибка, введите пятизначное число");  
     }
 }
+
